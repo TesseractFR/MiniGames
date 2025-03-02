5c7870f021d0a69db27c4491ef44af5510b1d570
aikiboo <florian.suchaud@hotmail.fr>
2025-03-02 13:52:00 +0100

    Init

---
+++ src/main/kotlin/onl/tesseract/miniGames/MiniGames.kt
@@ -0,0 +1,12 @@
+package onl.tesseract.miniGames
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MiniGames : JavaPlugin() {
+
+    override fun onEnable() { // Plugin startup logic
+    }
+
+    override fun onDisable() { // Plugin shutdown logic
+    }
+}
