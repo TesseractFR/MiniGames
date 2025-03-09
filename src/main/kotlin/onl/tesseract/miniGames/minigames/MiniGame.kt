c8118ed31ed6de5b2b26d60660e245e483ebedd2
aikiboo <florian.suchaud@hotmail.fr>
2025-03-09 19:33:25 +0100

    Ajout de la classe Mère des Minijeux

---
+++ src/main/kotlin/onl/tesseract/miniGames/minigames/MiniGame.kt
@@ -0,0 +1,52 @@
+package onl.tesseract.miniGames.minigames
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
+import onl.tesseract.miniGames.utils.MINIGAMES_GAMES_FOLDER_NAME
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.configuration.file.YamlConfiguration
+import java.io.File
+
+
+private val defaultExplicationComponent = Component.text(" ---------", NamedTextColor.GOLD)
+        .appendNewline()
+        .append(Component.text(" MINIGAME ", NamedTextColor.YELLOW))
+        .appendNewline()
+        .append(Component.text(" ----------", NamedTextColor.GOLD))
+
+abstract class MiniGame(val name : String) {
+
+    private val pathFile = "$MINIGAMES_GAMES_FOLDER_NAME/$name.yaml"
+    protected val TUTO_MESSAGE_KEY = "Tutorial"
+    protected val MAPS_KEY = "Maps"
+    val maps = mutableMapOf<String,MiniGameMap>()
+
+    lateinit var explicationComponent : Component;
+
+    init {
+        loadConfig()
+    }
+
+    protected fun loadConfig() : YamlConfiguration {
+        val yamlConfiguration = YamlConfiguration.loadConfiguration(File(pathFile))
+        explicationComponent = yamlConfiguration.getComponent(TUTO_MESSAGE_KEY, GsonComponentSerializer.gson())?:defaultExplicationComponent
+        yamlConfiguration.getConfigurationSection(MAPS_KEY)?.let { loadMaps(it) }
+        return yamlConfiguration
+    }
+
+    fun saveConfig(){
+        File(pathFile).let {
+            val yamlConfiguration = YamlConfiguration.loadConfiguration(it)
+            yamlConfiguration.setComponent(TUTO_MESSAGE_KEY,GsonComponentSerializer.gson(),explicationComponent )
+            saveMaps(yamlConfiguration.createSection(MAPS_KEY))
+            yamlConfiguration.save(it)
+
+        }
+
+    }
+
+    protected abstract fun loadMaps(configuration: ConfigurationSection)
+    protected abstract fun saveMaps(createSection: ConfigurationSection)
+
+}
