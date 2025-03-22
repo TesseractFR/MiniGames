c95a00df1450d1ab6edc73c1db5d94b3d38574bf
aikiboo <florian.suchaud@hotmail.fr>
2025-03-22 21:30:44 +0100

    Retrait JoinCommand.kt inutile

--- src/main/kotlin/onl/tesseract/miniGames/command/MinigamesCommand.kt
+++ src/main/kotlin/onl/tesseract/miniGames/command/MinigamesCommand.kt
@@ -2,7 +2,6 @@ package onl.tesseract.miniGames.command
 
 import onl.tesseract.commandBuilder.CommandContext
 import onl.tesseract.commandBuilder.annotation.Command
-import onl.tesseract.miniGames.command.basic.JoinCommand
 import onl.tesseract.miniGames.command.basic.TntRunCommand
 
 @Command(
--- src/main/kotlin/onl/tesseract/miniGames/command/basic/JoinCommand.kt
+++
@@ -1,8 +0,0 @@
-package onl.tesseract.miniGames.command.basic
-
-import onl.tesseract.commandBuilder.CommandContext
-import onl.tesseract.commandBuilder.annotation.Command
-
-@Command(name = "join")
-class JoinCommand : CommandContext(){
-}
